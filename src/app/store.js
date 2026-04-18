3b089d8b2b9032bfc8e91dfedc851e2c489bf477
Emmanuel B <blancocemmanuel@gmail.com>
2026-04-18 18:15:47 +1000

    commits

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
+import articlesReducer from "../features/articles/articlesSlice";
+
 
 export default configureStore({
   reducer: {
-    articles: articlesReducer,
-    comments: commentsReducer,
+    articles: articlesReducer
+
 
   },
 });
--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -1,10 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 
-export const loadAllPreviews = createAsyncThunk(
-  'articlePreviews/loadAllPreviews',
+export const loadArticles = createAsyncThunk(
+  'articles/loadArticles', //articles=from slice name: and loadArticles thunk name
   async () => {
-    const data = await fetch('api/articles');
+    const data = await fetch('https://dev.to/api/articles');
     const json = await data.json();
     return json;
   }
@@ -14,26 +14,54 @@ export const loadAllPreviews = createAsyncThunk(
 
 
 export const articlesSlice = createSlice({
-  name: 'articlePreviews',
+  name: 'articles',
   initialState: {
-    articles: [],
-    isLoadingArticlePreviews: false,
-    hasError: false
+    articlesList: [],
+    isLoading: false,
+    error: false
   },
   extraReducers: (builder) => {
     builder
-      .addCase(loadAllPreviews.pending, (state) => {
-        state.isLoadingArticlePreviews = true;
-        state.hasError = false;
+      .addCase(loadArticles.pending, (state) => {
+        state.isLoading = true;
+        state.error = false;
       })
-      .addCase(loadAllPreviews.fulfilled, (state, action) => {
-        state.isLoadingArticlePreviews = false;
-        state.articles = action.payload;
+      .addCase(loadArticles.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.articlesList = action.payload;
       })
-      .addCase(loadAllPreviews.rejected, (state, action) => {
-        state.isLoadingArticlePreviews = false;
-        state.hasError = true;
+      .addCase(loadArticles.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = true;
         
       })
   },
 });
+
+export default articlesSlice.reducer;
+/*What’s happening:
+You write:
+initial state
+reducers / extraReducers
+createSlice combines all of that and builds the reducer for you
+So:
+you don’t see the reducer 
+Redux Toolkit generates it internally */
+
+
+
+
+
+
+// 1. did upper slice part
+// 2. did thunk
+// 3. did extrareducers in slice
+
+//REMEMBER:
+/*Store state (global state)
+→ contains all slices (articles, comments, etc.)
+Slice state (inside reducer)
+→ contains only that slice’s data (articlesList, isLoading, error)
+
+👉 In components (useSelector) → use full state (state.articles...)
+👉 In slice reducers → use slice state directly (state...)*/
