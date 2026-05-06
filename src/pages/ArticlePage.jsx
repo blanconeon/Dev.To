00f07f2483c240cc1c1cc1ceeea137abc8896596
Emmanuel B <blancocemmanuel@gmail.com>
2026-05-06 20:41:24 +1000

    good sesh

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,18 +1,37 @@
-import { selectCurrentArticle, selectIsLoading  } from "../features/articles/articlesSlice";
+import { loadArticlesById, selectCurrentArticle, selectIsLoading  } from "../features/articles/articlesSlice";
 import { useDispatch, useSelector } from 'react-redux';
-import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 
 
-const ArticlePage = ({article}) => {
+const ArticlePage = () => {
+const dispatch = useDispatch();
+const loadedArticle = useSelector(selectCurrentArticle);
+const isLoadingArticles = useSelector(selectIsLoading);
+
+const {id} = useParams(); //useParams returns an objects because there could be multiple parameters, like: /users/:userId/posts/:postId - this would give { userId: "...", postId: "..."}. IMPORTANT: the variable name must match the key to hold that value. In my case my parameter's key is id so I label the variable id. 
+
+useEffect(() => {
+if (id) {
+    dispatch(loadArticlesById(id))
+ }
+ 
+},[id])
+
+if (isLoadingArticles) {
+    return <div>Is Loading</div>
+}
+
+if (typeof loadedArticle !== "object" || loadedArticle === null || Array.isArray(loadedArticle) || Object.keys(loadedArticle).length === 0) {
+return <div>No results</div>
+}
+
 
 
 return (
 <>
-<div>
-   
-</div>
-
+<div dangerouslySetInnerHTML={{ __html: loadedArticle.body_html }}/>
 
 </>
 )}  ////// you are making this componnet bring all imports dont forget that the link in articleCard changes the URL, so here you read the url with params and call the new thunk. follow the pattern in HomePage. 
