af4576f122f42dff7b5dae746a2105f9c04c2ccd
Emmanuel B <blancocemmanuel@gmail.com>
2026-05-24 19:03:06 +1000

    commit

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchedProfile, selectedProfIsLoading, loadProfileByUserName } from "../features/profile/profileSlice";
+import { selectIsLoading, allArticles, loadArticlesByUsername } from "../features/articles/articlesSlice";
+import ArticleCard from "../components/ArticleCard";
 
 import { useEffect } from "react";
 
@@ -11,21 +13,40 @@ const ProfilePage = () => {
 const dispatch = useDispatch();
 const loadedProfile = useSelector(fetchedProfile);
 const profileIsLoading = useSelector(selectedProfIsLoading);
-
+//from articlesSlice
+const loadedArticles = useSelector(allArticles)
 
 const {username} = useParams();
 
 useEffect(()=> {
 if (username) {
     dispatch(loadProfileByUserName(username))
+    dispatch(loadArticlesByUsername(username))
 }
 }, [username])
 
+if (profileIsLoading){
+    return <div>Is loading..</div>
+}
+
+if (!loadedProfile) {
+    return <div>No results</div>
+}
+
 return (
 <>
-
-
-
+<h2>{loadedProfile.username}</h2>
+<h3>Github: {loadedProfile.github_username}</h3>
+<img src={loadedProfile.profile_image} alt="photo"/>
+<h5>{loadedProfile.location}</h5>
+<h5>Joined on {loadedProfile.joined_at}</h5>
+<h4>About me: {loadedProfile.summary}</h4>
+<hr />
+<div>
+    {loadedArticles.map(article => (
+        <ArticleCard key={article.id} article={article} />
+    ))}
+</div>
 
 </>
 )}
